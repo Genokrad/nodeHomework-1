b407c797957446ed650d5a584b177f879ef4092a
Genokrad <zeeper90@gmail.com>
2023-03-08 02:03:22 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,83 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    // console.table(JSON.parse(contacts));
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    const some = JSON.parse(contacts).find((contact) => {
+      if (String(contact.id) === String(contactId)) {
+        return contact;
+      }
+    });
+    console.log(some);
+    return some;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const removeContact = async (contactId) => {
+  try {
+    // const contacts = await listContacts();
+
+    // const result = contacts.findIndex(
+    //   (contact) => String(contact.id) === String(contactId)
+    // );
+
+    // if (result === -1) {
+    //   return null;
+    // }
+
+    // contacts.splice(result, 1);
+
+    // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    // return contacts;
+    const contacts = await fs.readFile(contactsPath, "utf-8");
+    const filteredContacts = JSON.parse(contacts).filter(
+      (contact) => contact.id !== String(contactId)
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
+    console.table(filteredContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function addContact(name, email, phone) {
+  try {
+    // const contacts = await listContacts();
+    // const newContact = { id: uuidv4(), name, email, phone };
+    // contacts.push(newContact);
+    // await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    // return contacts;
+
+    const contacts = await listContacts();
+    const updatedContacts = [...contacts, { id: uuidv4(), name, email, phone }];
+    console.table(updatedContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+    return updatedContacts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,68 @@
+const contactActions = require("./contacts");
+const { program } = require("commander");
+
+contactActions.listContacts();
+async function getAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const listContacts = await contactActions.listContacts();
+      console.table(listContacts);
+      break;
+    case "get":
+      contactActions.getContactById(id);
+      break;
+    case "add":
+      contactActions.addContact(name, email, phone);
+      break;
+    case "remove":
+      contactActions.removeContact(id);
+
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+// console.log(options);
+getAction(options);
+
+// getAction({ action: "list" });
+// getAction({ action: "getById", id: "11" });
+// getAction({ action: "deleteById", id: "11" });
+// getAction({
+//   action: "addContact",
+//   name: "Alec Howard",
+//   email: "Donec.elementum@scelerisquescelerisquedui.net",
+//   phone: "(748) 206-2688",
+// });
+
+// const actionIndex = process.argv.indexOf("--action");
+// const action = process.argv[actionIndex + 1];
+
+// console.log(process.argv);
+// console.log(actionIndex);
+
+// // if (actionIndex !== -1) {
+// //   getAction({ action, id });
+// // } else {
+// //   console.log("unknown action");
+// // }
+
+// if (actionIndex !== -1) {
+//   if (action === "getById") {
+//     const id = process.argv[actionIndex + 2];
+//     getAction({ action, id });
+//   }
+// } else {
+//   console.log("unknown action");
+// }
